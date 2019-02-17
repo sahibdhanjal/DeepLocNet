767fcb96ab605578f43f340ba0f3e50a4eb1be96
Sahib Dhanjal <dhanjalsahib@gmail.com>
2019-02-17 03:26:17 -0500

    DNN architectures

--- include/DNN.py
+++ include/DNN.py
@@ -37,3 +37,55 @@ class DNN(nn.Module):
     def forward(self, x):
         res = self.autoenc(x)
         return res
+
+
+class DNN2(nn.Module):
+    def __init__(self):
+        super(DNN2, self).__init__()
+
+        self.autoenc = nn.Sequential(
+            nn.Linear(2, 10),
+            nn.ReLU(),
+            nn.Linear(10, 20),
+            nn.ReLU(),
+            nn.Linear(20, 50),
+            nn.ReLU(),
+            nn.Linear(50, 100),
+            nn.ReLU(),
+            nn.Linear(100, 50),
+            nn.ReLU(),
+            nn.Linear(50, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Linear(10, 2),
+            nn.ReLU(),
+            nn.Softmax(dim=-1),
+        )
+
+    def forward(self, x):
+        res = self.autoenc(x)
+        return res
+
+
+class DNN3(nn.Module):
+    def __init__(self):
+        super(DNN3, self).__init__()
+
+        self.autoenc = nn.Sequential(
+            nn.Linear(2, 10),
+            nn.ReLU(),
+            nn.Linear(10, 20),
+            nn.ReLU(),
+            nn.Linear(20, 50),
+            nn.ReLU(),
+            nn.Linear(50, 100),
+            nn.ReLU(),
+            nn.Linear(100, 2),
+            nn.ReLU(),
+            nn.Softmax(dim=-1),
+        )
+
+    def forward(self, x):
+        res = self.autoenc(x)
+        return res
